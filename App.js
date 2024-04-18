a5d6e1dde77585205a41ceb5df93e1c4d92dbe52
BRIZOLA99 <129482329+BRIZOLA99@users.noreply.github.com>
2024-04-18 14:07:39 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, StyleSheet, Button, Text } from 'react-native';
+import { View, TextInput, StyleSheet, Button, Text, Animated } from 'react-native';
 
 // Définir la fonction generateEmptyGrid avant le composant Grid
 const generateEmptyGrid = () => {
@@ -11,6 +11,14 @@ const generateEmptyGrid = () => {
     }
     grid.push(row);
   }
+
+  // Ajouter les indices dans la grille
+  grid[0][0].value = '1.';
+  grid[1][0].value = '2.';
+  grid[2][1].value = '3.';
+  grid[3][1].value = '4.';
+  grid[8][1].value = '5.';
+
   return grid;
 };
 
@@ -20,14 +28,16 @@ const Grid = () => {
   const [message, setMessage] = useState('');
   const [score, setScore] = useState(0);
   const [grid, setGrid] = useState(generateEmptyGrid()); // Utilisation de la fonction generateEmptyGrid ici
+  const [slideAnim] = useState(new Animated.Value(0)); // Nouvel état pour l'animation
 
-  const words = [
-    { id: 1, word: 'REACT', clue: 'Framework JavaScript', x: 0, y: 0, across: true, background: 'lightblue', found: false },
-    { id: 2, word: 'NATIVE', clue: 'Framework pour les applications mobiles', x: 0, y: 1, across: false, background: 'lightgreen', found: false },
-    { id: 3, word: 'CROISE', clue: 'Type de jeu de mots', x: 1, y: 1, across: true, background: 'lightyellow', found: false },
-    { id: 4, word: 'GRILLE', clue: 'Structure de jeu de mots', x: 1, y: 2, across: false, background: 'lightpink', found: false },
-    { id: 5, word: 'JAVASCRIPT', clue: 'Langage de programmation', x: 1, y: 8, across: true, background: 'lightsalmon', found: false },
-  ];
+  // Définir les mots à trouver
+  const [words, setWords] = useState([
+    { id: 1, word: 'REACTA', clue: 'Framework JavaScript', x: 0, y: 0, across: true, background: 'lightblue', isFound: false },
+    { id: 2, word: 'NATIVE', clue: 'Framework pour les applications mobiles', x: 0, y: 1, across: false, background: 'lightgreen', isFound: false },
+    { id: 3, word: 'CROISE', clue: 'Type de jeu de mots', x: 1, y: 1, across: true, background: 'lightyellow', isFound: false },
+    { id: 4, word: 'GRILLE', clue: 'Structure de jeu de mots', x: 1, y: 2, across: false, background: 'lightpink', isFound: false },
+    { id: 5, word: 'JAVASCRIPT', clue: 'Langage de programmation', x: 1, y: 8, across: true, background: 'lightsalmon', isFound: false },
+  ]);
 
   // Fonction appelée lorsque l'utilisateur change la valeur de la case
   const handleInputChange = (text) => {
@@ -47,11 +57,13 @@ const Grid = () => {
     );
     if (selectedWord && answer === selectedWord.word) {
       setMessage('Bonne réponse!');
-      if (!selectedWord.isUnused) { // Vérifier si le mot n'a pas déjà été trouvé
-        setScore(score + 1); // Incrémenter le score si le mot n'a pas déjà été trouvé
-        selectedWord.isUnused = true; // Marquer le mot comme trouvé
+      if (!selectedWord.isFound) {
+        setScore(score + 1);
+        selectedWord.isFound = true;
+        setWords([...words]);
       }
       updateGrid(selectedWord, words.indexOf(selectedWord));
+      slideWord(); // Appeler la fonction pour lancer l'animation
     } else {
       setMessage('Mauvaise réponse!');
     }
@@ -65,36 +77,28 @@ const Grid = () => {
       const col = word.across ? word.x + i : word.x;
       newGrid[row][col].value = i === 0 ? `${index + 1}.${word.word[i]}` : word.word[i];
       newGrid[row][col].isCorrect = true;
-      newGrid[row][col].background = word.background; // Définir l'arrière-plan de la cellule
+      newGrid[row][col].background = word.background;
     }
     setGrid(newGrid);
-    renderCluesInGrid();
   };
 
-  // Fonction pour afficher les indices à côté de la première lettre de chaque mot
-  const renderCluesInGrid = () => {
-    words.forEach((word) => {
-      const row = word.across ? word.y : word.y + 1;
-      const col = word.across ? word.x + 1 : word.x;
-      if (!word.found) {
-        grid[row][col].value = `${word.id}.`;
-      } else {
-        for (let i = 0; i < word.word.length; i++) {
-          const row = word.across ? word.y : word.y + i;
-          const col = word.across ? word.x + i : word.x;
-          grid[row][col].value = '';
-        }
-      }
+  // Fonction pour animer le mot entré
+  const slideWord = () => {
+    Animated.timing(slideAnim, {
+      toValue: 1,
+      duration: 500, // Durée de l'animation en millisecondes
+      useNativeDriver: false,
+    }).start(() => {
+      // Réinitialiser l'animation après qu'elle soit terminée
+      slideAnim.setValue(0);
+      // Réinitialiser la réponse
+      setAnswer('');
     });
   };
 
-  // Appeler la fonction pour afficher les indices dans la grille lors du montage du composant
-  useEffect(() => {
-    renderCluesInGrid();
-  }, []);
-
-  return (
-    <View style={styles.container}>
+  // Fonction pour afficher la grille
+  const renderGrid = () => {
+    return (
       <View style={styles.grid}>
         {grid.map((row, rowIndex) => (
           <View key={rowIndex} style={styles.row}>
@@ -113,20 +117,50 @@ const Grid = () => {
           </View>
         ))}
       </View>
+    );
+  };
+
+  // Fonction pour afficher les indices des mots à trouver
+  const renderClues = () => {
+    return (
       <View style={styles.cluesContainer}>
         <Text style={styles.cluesTitle}>Indices :</Text>
-        {words.map(({ clue }, index) => (
-          <Text key={index} style={styles.clue}>{`${index + 1}. ${clue}`}</Text>
+        {words.map(({ clue, isFound }, index) => (
+         <Text key={index} style={styles.clue}>{isFound ? <Text style={{textDecorationLine: 'line-through'}}>{`${index + 1}. ${clue}`}</Text> : `${index + 1}. ${clue}`}</Text>
         ))}
       </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderGrid()}
+      {renderClues()}
+      <Animated.Text
+        style={[
+          styles.input,
+          {
+            transform: [
+              {
+                translateY: slideAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, -100], // Réglez la hauteur de déplacement de votre choix
+                }),
+              },
+            ],
+          },
+        ]}
+      >
+        {answer}
+      </Animated.Text>
       <TextInput
-        style={styles.input}
+        style={styles.inputHidden} // Style caché pour la saisie de texte
         value={answer}
         onChangeText={handleInputChange}
         placeholder="Entrez votre réponse"
       />
       <Button title="Soumettre" onPress={checkAnswer} />
-      <Text style={styles.message}>{message}</Text>
+      <Text style={[styles.message, message === 'Bonne réponse!' ? { color: 'green' } : { color: 'red' }]}>{message}</Text>
       <Text style={styles.score}>Score: {score}</Text>
     </View>
   );
@@ -141,9 +175,8 @@ const Cell = ({ value, isBlack, isCorrect, background, isSelected, isUnused, onS
   } else if (isSelected) {
     cellStyles.push(styles.selectedCell);
   } else if (isUnused) {
-    cellStyles.push(styles.unusedCell); // Style pour les cellules inutilisées
+    cellStyles.push(styles.unusedCell);
   }
-  // Ajouter l'arrière-plan dynamique à la cellule
   cellStyles.push({ backgroundColor: background });
   return (
     <Text style={cellStyles} onPress={onSelect}>
@@ -184,7 +217,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
   },
   unusedCell: {
-    backgroundColor: 'black', // Style pour les cellules inutilisées
+    backgroundColor: 'black',
   },
   cluesContainer: {
     marginBottom: 20,
@@ -198,10 +231,15 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   input: {
+    position: 'absolute', // Position absolue pour l'animation
     borderWidth: 1,
     marginBottom: 10,
     padding: 5,
     width: 200,
+  },
+  inputHidden: {
+    position: 'absolute', // Position absolue pour cacher la saisie de texte
+    opacity: 0,
   },
   message: {
     marginTop: 10,
